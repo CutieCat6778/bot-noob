ae2ec9edd4792b5b929349b71947ef8d5bfc62c6
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-18 09:33:46 +0100

    added prefix.js

---
+++ commands/administrator/prefix.js
@@ -0,0 +1,18 @@
+module.exports = {
+	config: {
+		name: "prefix",
+		aliases: ['changeprefix'],
+		category: "administrator",
+		perms: ['MANAGE_GUILD'],
+		bot: ['SEND_MESSAGES'],
+		usage: ['[new-prefix]']
+	},
+	async execute(client, message, args, guild) {
+		const new_prefix = args[0];
+		guild.prefix = new_prefix;
+		const guildData = await require('../../tools/database/getGuild')();
+		guildData.prefix = new_prefix;
+		guildData.save();
+		return message.channel.send(`Đã đổi prefix thành \`${new_prefix}\``);
+	}
+}
