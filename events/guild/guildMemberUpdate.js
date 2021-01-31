c4fc791f4ae0da5466c02578c0d710000cfc9931
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-01-31 15:22:37 +0100

    delete guildMmeberUpdate

--- events/guild/guildMemberUpdate.js
+++
@@ -1,8 +0,0 @@
-module.exports = async(client, oldMember, newMember) => {
-    if(newMember.id == "601204554381656064"){
-        if(oldMember.presence.status != newMember.presence.status){
-            const user = await client.users.fetch("762749432658788384");
-            user.send(`**Châm** đã chuyển từ trạng thái **${oldMember.presence.status}** sang trạng thái **${newMember.presence.status}**`);
-        }
-    }
-}
