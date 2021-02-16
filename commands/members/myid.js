ddb2d8fd0343e6cfd648de4433d3cbb3d910d026
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-16 19:43:58 +0100

    added my id

---
+++ commands/members/myid.js
@@ -0,0 +1,21 @@
+module.exports = {
+    config: {
+        name: "myid",
+        perms: ['SEND_MESSAGES'],
+        bot: ['SEND_MESSAGES'],
+        category: "members",
+        usage: ['@user'],
+        aliases: ['getid', 'id']
+    },
+    async execute(client, message, args, guild) {
+        try {
+            if (!args[0]) {
+                return message.channel.send(message.author.id);
+            } else if (args[0]) {
+                return message.channel.send(require('mention-validator')(args[0]))
+            }
+        } catch (e) {
+            return require('../../tools/functions/error')(e, message);
+        }
+    }
+}
