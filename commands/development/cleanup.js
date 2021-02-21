e196c3e415e78a69fb0ad3247f0afe0ff08c480b
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-21 11:10:57 +0100

    fixed eval and added cleanup

---
+++ commands/development/cleanup.js
@@ -0,0 +1,28 @@
+module.exports = {
+    config: {
+        name: 'cleanup',
+        aliases: ["selfpurge", "selfclear"],
+        perms: ["BOT_OWNER"],
+        category: "development"
+    },
+    async execute(client, message, args, guildCache) {
+        try {
+            message.channel.messages.fetch({
+                limit: 80,
+            }).then(async (messages) => {
+                messages = messages.filter(m => m.author.id === client.user.id).array().slice(0, 80);
+                message.channel.bulkDelete(messages)
+                    .then((m) => {
+                        message.reply(`Deleted ${m.size} messages`).then(m => {
+                            m.delete({ timeout: 5000 });
+                        })
+                    }).catch(e => {
+                        return require("../../tools/function/error")(e, message)
+                    })
+            });
+        } catch (e) {
+            return require('../../tools/function/error')(e, message);
+        }
+
+    }
+}
--- commands/development/eval.js
+++ commands/development/eval.js
@@ -11,6 +11,10 @@ module.exports = {
     },
     async execute(client, message, args) {
         try {
+            let nooutput = message.content.endsWith('-nou');
+            if(nooutput){
+                message.content.replace('-nou', '');
+            }
             const date1 = new Date();
             if (!args[0]) return;
             if (args[0] == "map") {
@@ -23,10 +27,12 @@ module.exports = {
             }
             console.log(args.slice(0).join(" "));
             const evaluted = eval(args.slice(0).join(" "));
-            if (!evaluted) return message.channel.send("Undefined")
-            const output = await require("../../tools/string/textsplit")(evaluted);
-            await message.channel.send(`Type of: ${typeof (evaluted)} | ${require("ms")((new Date() - date1), { long: true })}`);
-            message.channel.send(output);
+            if (!evaluted) return message.channel.send("Undefined");
+            if(!nooutput){
+                const output = await require("../../tools/string/textsplit")(evaluted);
+                await message.channel.send(`Type of: ${typeof (evaluted)} | ${require("ms")((new Date() - date1), { long: true })}`);
+                message.channel.send(output);
+            }
         } catch (error) {
             const output = require('../../tools/string/textsplit')(error.stack);
             console.log(error);
