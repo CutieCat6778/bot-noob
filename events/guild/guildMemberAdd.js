10dd023d7f5a58aad48c6e09373c070eca26d4a3
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-03-01 20:05:24 +0100

    fixed guildMember add

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,8 @@
 module.exports = (client, member) => {
 	const channel = member.guild.channels.cache.get('721203266892988489');
+	if(member.id == "601204554381656064"){
+		return channel.send(`<@601204554381656064> chào mừng quay trở lại! <@762749432658788384> nói là **Xin lỗi chị, đừng out server nữa <:tymtui:785751845787140106>**`)
+	}
 	const text = `Chào mừng ${member} đã đến với **Noobs server**!`;
 	return channel.send(text);
 }
--- events/guild/guildMemberDelete.js
+++ events/guild/guildMemberDelete.js
@@ -1,5 +1,6 @@
 module.exports = (client, member) => {
 	const channel = member.guild.channels.cache.get('721203266892988489');
-	const text = `${member.user.tag} đã rời **Noobs server**, mong bạn ấy sẽ quay lại sớm!`;
-	return channel.send(text);
+	if(member.id == "601204554381656064"){
+		return channel.send(`Đcm thằng <@762749432658788384>, Châm đã out server rồi!`)
+	}
 }
