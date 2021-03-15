b18e9170e6576d57cd26bd04754927dca251b720
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-03-15 08:00:10 +0100

    fixed color

--- events/client/ready.js
+++ events/client/ready.js
@@ -19,8 +19,8 @@ module.exports = async (client) => {
         let counter = 0;
         client.setInterval(() => {
             const role = client.guilds.cache.get("721203266452586507").roles.cache.get("820904733455024141");
-            const colors = ["#ee4035", "#f37736", "#fdf498", "#7bc043", "#0392cf"];
-            if(counter == 4) counter = 0;
+            const colors = ["#ee1c25", "#f26521", "#f7941d", "#ffc20f", "#fef200", "#cadb2a", "#8ec63f", "#3ab549", "#01a553", "#00a674", "#01a89e", "#00abc5", "#008fd5", "#0072bb", "#0054a5", "#2e3192", "#662e91", "#92278f", "#92278f", "#bd1a8d", "#ed008c", "#ec0974", "#ed145b", "#ed1941"];
+            if(counter == colors.length - 1) counter = 0;
             role.setColor(colors[counter]);
             counter++;
         }, 10000)
