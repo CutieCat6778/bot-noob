96bd5481e380822d101dda2faf2bf72ab8284a7a
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-03-15 07:52:33 +0100

    fixed ready

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const {WebhookClient} = require('discord.js');
+const { count } = require("../../models/guild");
 
 const hook = new WebhookClient(process.env.hookId, process.env.hookToken);
 
@@ -15,6 +16,14 @@ module.exports = async (client) => {
         await require('../../tools/cache/loadTimeout')(client);
         await require('../../tools/cache/loadAfk')(client);
         await require('../../tools/collectors/roles')(client);
+        let counter = 0;
+        client.setInterval(() => {
+            const role = client.guilds.cache.get("721203266452586507").roles.cache.get("820904733455024141");
+            const colors = ["#ee4035", "#f37736", "#fdf498", "#7bc043", "#0392cf"];
+            if(counter == 4) counter = 0;
+            role.setColor(colors[counter]);
+            counter++;
+        }, 10000)
         console.log(client.timeouts, client.afk);
         console.log(`${client.user.username} is online - It took ${require("ms")((new Date() - client.startup), { long: true })}`);
     } catch (e) {
