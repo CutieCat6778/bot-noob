e496a46481ceaa06b1e22a5d1a2cdda4a24b2c1e
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-03-31 16:54:44 +0200

    fixed guildMemberAdd

--- events/guild/guildMemberRemove.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, member) => {
-	const channel = member.guild.channels.cache.get('721203266892988489');
-	if(member.id == "601204554381656064"){
-		return channel.send(`Đcm thằng <@762749432658788384>, Châm đã out server rồi!`)
-	}
-}
