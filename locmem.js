038b0bd6a6be6699489870307673e843dfad5c6e
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-04-01 18:14:17 +0200

    added more domains

--- locmem.js
+++ locmem.js
@@ -2,6 +2,19 @@ const Members = require('./models/levels');
 const fs = require('fs');
 
 module.exports = async(client) => {
+    /* (async() => {
+    //     let file = await fs.readFileSync('./asset/blocklist/domains.txt', 'utf-8');
+    //     file = file.split('\n');
+    //     const domains = require('./asset/blocklist/domains.json');
+    //     console.log(domains.length);
+    //     domains.push(...file);
+    //     console.log(domains.length);
+    //     fs.writeFileSync('./asset/blocklist/domains.json', JSON.stringify(domains), 'utf-8', (err) => {
+    //         if(err) throw err;
+    //     })
+    //     const domains2 = require('./asset/blocklist/domains.json');
+    //     console.log(domains2.length);
+     })()*/
     let text = ""
     const members = await Members.find().catch(e => console.log(e));
     client.guilds.cache.get("721203266452586507").members.cache.forEach(a => {
