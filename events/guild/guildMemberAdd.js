ca75c09e1770ff993354be1072e211537332369f
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-04-26 08:33:11 +0200

    fixed guildMemberAdd

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,11 +1,20 @@
 module.exports = async(client, member) => {
 	const newInvites = await member.guild.fetchInvites();
+	let found = false;
 	newInvites.forEach(newInvite => {
 		let invite = client.invites.get(newInvite.code)
-		if(newInvite.uses > invite.uses) {
+		if((newInvite.uses > invite.uses) && (newInvite.code == invite.code)) {
+			found = true;
 			const channel = member.guild.channels.cache.get('721203266892988489');
 			const user = member.guild.members.cache.get(newInvite.inviter.id);
 			const text = `Chào mừng ${member} đã đến với **Động Noobs**. Được mời bởi **${user.user.tag}**!`;
+			return channel.send(text);
+		}
+	}).then(() => {
+		if(!found){
+			const channel = member.guild.channels.cache.get('721203266892988489');
+			const user = member.guild.members.cache.get(newInvite.inviter.id);
+			const text = `Chào mừng ${member} đã đến với **Động Noobs**!`;
 			return channel.send(text);
 		}
 	})
