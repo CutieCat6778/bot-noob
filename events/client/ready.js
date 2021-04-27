ab89425e43a37555c14331de328325fa1d2c4c80
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-04-27 10:35:42 +0200

    added dynamic status

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,7 +5,7 @@ const hook = new WebhookClient(process.env.hookId, process.env.hookToken);
 
 module.exports = async (client) => {
     try {
-        await client.user.setActivity(`các thành viên Noobs`, { type: "WATCHING" });
+        await client.user.setActivity(`các thành viên Noobs`);
         await mongoose.connect(process.env.mongo, {
             useNewUrlParser: true,
             useUnifiedTopology: true
@@ -18,8 +18,26 @@ module.exports = async (client) => {
         await require('../../tools/cache/loadInvites')(client);
         //await require('../../tools/collectors/pages')(client);
         //await require('../../tools/converter/txtToArray')();
-        console.log(client.timeouts, client.afk, client.invites);
+        console.log(client.timeouts, client.afk);
         console.log(`${client.user.username} is online - It took ${require("ms")((new Date() - client.startup), { long: true })}`);
+        const text = [
+            `đang xem ${client.users.cache.size} người dùng`,
+            `đang ngồi xem ai đó post link bậy`,
+            `xem ai đó react sao vàng`,
+            `thả thính là đam mê`,
+            `Nếu bị hỏng thì gọi thằng Cat`,
+            `noobs là một cộng đồng`,
+            `vào luật để đọc luật`,
+            `nếu có vấn đề, ib cho mod hoặc Cat`,
+            `ngồi xem mấy con giời xàm lờ`,
+            `người ăn cơm tró thường niên`,
+            `hãy vui lên vì đời cho phép`
+        ]
+        let i = 0;
+        client.setInterval(() => {
+            client.user.setActivity(text[i]);
+            i == text.length - 1 ? i = 0 : i++;
+        }, 15000);
     } catch (e) {
         return require("../../tools/functions/error")(e, undefined)
     }
