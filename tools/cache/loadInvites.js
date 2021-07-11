3ef924917105f93fd18bf1f72c89468e8f05a8e5
CutieCat6778 <thinhnguyenhuhung@gmail.com>
2021-07-11 18:59:23 +0200

    added report

--- tools/cache/loadInvites.js
+++ tools/cache/loadInvites.js
@@ -4,7 +4,6 @@ module.exports = async (client) => {
         const guild = client.guilds.cache.get('721203266452586507');
         let invites = await guild.fetchInvites();
         for (let [key, value] of invites.entries()) {
-            console.log(value.uses)
             if (guild.vanityURLCode) client.invites.set(guild.vanityURLCode, await guild.fetchVanityData());
             value.code ? client.invites.set(key, value) : null;
         }
