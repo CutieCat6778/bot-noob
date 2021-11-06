b979062d728c9e0ee28adaa38f978f03da9e88d6
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-11-06 16:25:52 +0100

    added intents

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const client = new Client({
         Intents.FLAGS.GUILDS, 
         Intents.FLAGS.GUILD_MEMBERS, 
         Intents.FLAGS.GUILD_MESSAGES, 
-        Intents.FLAGS.GUILD_VOICE_STATES,
         Intents.FLAGS.GUILD_WEBHOOKS,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.GUILD_EMOJIS_AND_STICKERS
     ],
     allowedMentions: { parse: ['users', 'roles'], repliedUser: true }   
 });
