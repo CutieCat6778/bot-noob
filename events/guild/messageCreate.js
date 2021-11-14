7ffda10b23bb3ff40cfce6c1baf8484217c21cb6
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-11-14 15:31:56 +0100

    added some url

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -15,8 +15,8 @@ module.exports = async (client, message) => {
             if (message.content.includes('.') && message.content.includes('http')) {
                 for (let domain of blocklistdomains) {
                     if (!message.content.includes(domain)) {
-                        await message.reply('**đường link này đã bị chặn!!!**').then(m => m.delete({ timeout: 7000 }))
                         message.delete();
+                        message.channel.send('**đường link này đã bị chặn!!!**').then(m => m.delete({ timeout: 7000 }))
                         const channel = message.guild.channels.cache.get('813765397353725962');
                         channel.send({ embeds: [{ title: "Đã chặn được một tên miền!", description: `${message.author.id} | ${message.author.tag}\n\n${message.content.split('://').join('[://]').split('.').join('[.]')}` }] });
                     }
