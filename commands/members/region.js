90514ab3691ea0d39c0954a0d41b27a54dd38f11
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-12 20:19:26 +0100

    fixed category

---
+++ commands/members/region.js
@@ -0,0 +1,21 @@
+module.exports = {
+    config: {
+        name: "region",
+        perms: ['SEND_MESSAGES'],
+        bot: ['SEND_MESSAGES'],
+        aliases: ['server', 'doiserver', 'đổiserver'],
+        category: "members",
+        usage: ['[name]']
+    },
+    async execute(client, message, args, guildCache) {
+        try {
+            if (!message.member.voice.channel) return message.reply('bạn phải ở trong voice!');
+            if (!args[0]) return message.reply('xin thí chủ cho địa điểm (europe, brazil, hong kong, india, japan, rotterdam, russia, singapore, sydney');
+            const name = args.join(' ');
+            await message.member.voice.channel.setName(name);
+            return message.react('<:hmmmmm:770520614444335104>')
+        } catch (e) {
+            return require('../../tools/functions/error')(e, message);
+        }
+    }
+}
