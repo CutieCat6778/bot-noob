03d61e1d495001b56716f85d8289165360d9f11f
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-12 20:23:09 +0100

    fixed function name

--- commands/members/region.js
+++ commands/members/region.js
@@ -12,7 +12,7 @@ module.exports = {
             if (!message.member.voice.channel) return message.reply('bạn phải ở trong voice!');
             if (!args[0]) return message.reply('xin thí chủ cho địa điểm (europe, brazil, hong kong, india, japan, rotterdam, russia, singapore, sydney');
             const name = args.join(' ');
-            await message.member.voice.channel.setName(name);
+            await message.member.voice.channel.setRTCRegion(name);
             return message.react('<:hmmmmm:770520614444335104>')
         } catch (e) {
             return require('../../tools/functions/error')(e, message);
