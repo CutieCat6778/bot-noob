62b8a267c29b2d6fb76f407ed0207e48a94a649c
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-12 20:25:46 +0100

    fixed regions

--- commands/members/region.js
+++ commands/members/region.js
@@ -10,8 +10,9 @@ module.exports = {
     async execute(client, message, args, guildCache) {
         try {
             if (!message.member.voice.channel) return message.reply('bạn phải ở trong voice!');
-            if (!args[0]) return message.reply('xin thí chủ cho địa điểm (europe, brazil, hong kong, india, japan, rotterdam, russia, singapore, sydney');
-            const name = args.join(' ');
+            const regions = [null, 'us-west', 'us-east', 'us-central', 'us-south', 'singapore', 'southafrica', 'sydney', 'rotterdam', 'brazil', 'hongkong', 'russia', 'japan', 'india', 'south-korea'];
+            const name = args[0];
+            if(!regions.indexOf(name)) return message.reply(`những region chỉ có thể là \`${regions.join('\`, \`')}\``);
             await message.member.voice.channel.setRTCRegion(name);
             return message.react('<:hmmmmm:770520614444335104>')
         } catch (e) {
