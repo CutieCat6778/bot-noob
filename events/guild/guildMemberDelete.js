e461457775bc079f8b703a57dda47ae57386fdb3
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-16 15:31:34 +0100

    fixed update

--- events/guild/guildMemberDelete.js
+++ events/guild/guildMemberDelete.js
@@ -2,7 +2,7 @@ module.exports = async (client,member) => {
     const data = await require('../../tools/database/getLevel')(member.id);
     if(data){
         data.server.leave.push(new Date().getTime());
-        if((new Date(data.updates[-1])).getDate() != new Date().getDate()) data.updates.push(new Date().getTime());
+        if((new Date(data.updates[data.updates.length - 1]))).getDate() != new Date().getDate()) data.updates.push(new Date().getTime());
         await data.save();
     }
 }
