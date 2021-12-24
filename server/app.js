7472b5749d15d03d110e7f06dee20a5b56506266
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-12-24 21:18:22 +0100

    fixed origin

--- server/app.js
+++ server/app.js
@@ -4,17 +4,8 @@ const cors = require('cors');
 module.exports = (client) => {
   const app = express();
 
-  const whitelist = ['http://localhost:3001', 'https://my-profile-chi.vercel.app/']
-
   app.use(cors({
     origin: function(origin, callback){
-      // allow requests with no origin 
-      if(!origin) return callback(null, true);
-      if(whitelist.indexOf(origin) === -1){
-        var message = 'The CORS policy for this origin doesn\'t ' +
-                  'allow access from the particular origin.';
-        return callback(new Error(message), false);
-      }
       return callback(null, true);
     }
   }));
