a9d4dfb75e048e6c6a9371051430b45fc2e151c3
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-02-13 17:39:22 +0100

    fixed eval

--- commands/development/eval.js
+++ commands/development/eval.js
@@ -11,40 +11,13 @@ module.exports = {
     },
     async execute(client, message, args) {
         try {
-            let nooutput = message.content.endsWith('-nou');
-            if(nooutput){
-                args.pop();
-            }
-            const date1 = new Date();
             if (!args[0]) return;
-            if (args[0] == "map") {
-                const util = require("util");
-                const evaluted = eval(args.slice(1).join(" "));
-                const output = await require("../../tools/string/textsplit")(util.inspect(evaluted), true);
-                if (!output) return message.channel.send("Undefined")
-                await message.channel.send(`Type of: ${typeof (evaluted)} | ${require("ms")((new Date() - date1), { long: true })}`);
-                return message.channel.send(output);
-            }
-            console.log(args.slice(0).join(" "));
-            const evaluted = eval(args.slice(0).join(" "));
-            if(!nooutput){
-                console.log(evaluted)
-                const output = await require("../../tools/string/textsplit")(evaluted);
-                if (!output) return message.channel.send("Undefined")
-                await message.channel.send(`Type of: ${typeof (evaluted)} | ${require("ms")((new Date() - date1), { long: true })}`);
-                message.channel.send(output);
-            }
-        } catch (error) {
-            const output = require('../../tools/string/textsplit')(error.stack);
-            console.error(error);
-            let embed = new MessageEmbed()
-                .setColor("#40598F")
-                .addField("Eval", `
-                    \`\`\`console${output}\`\`\`
-                `)
-                .addField("command", `${message.content ? message.content : "Client error, no commands info"}`)
-                .setTimestamp()
-            return message.channel.send({embeds: [embed]});
+            const evaluted = eval(args.slice(0).join(' '));
+            if (!evaluted) return client.send(message, "Undefined");
+            const output = `\`\`\`json\n${require('../../utils/string/textSplit')(util.inspect(evaluted), true)}\n\`\`\``;
+            return client.send(message, output + `\n\`${typeof (evaluted)}\` -  **${require('ms')(new Date().getTime() - message.createdAt, { long: true })}**`);
+        } catch (e) {
+            return client.error(e);
         }
     }
 }
