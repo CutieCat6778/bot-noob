9f7d920da201c67b8705ea4cc1ed7b26e048b057
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-02-13 18:10:54 +0100

    added trai dep command

---
+++ commands/tools/trai.js
@@ -0,0 +1,42 @@
+const Flickr = require('flickr-sdk');
+const flickr = new Flickr(process.env.FLICKR_API_KEY);
+const MessageEmbed = require("../../classes/newEmbed")
+
+module.exports = {
+    config: {
+        name: "trai",
+        perms: ['SEND_MESSAGES'],
+        bot: ['SEND_MESSAGES'],
+        aliases: ['traixinh'],
+        category: "members"
+    },
+    async execute(client, message, args, guildCache) {
+        try {
+            flickr.photos.search({
+                text: 'Trai đẹp'
+            }).then(function (res) {
+                const body = res.body
+                if (body.stat != "ok") return new Error('API res stat is not ok!');
+                const photos = body.photos.photo;
+                const random = Math.floor(Math.random() * photos.length);
+                flickr.photos.getInfo({
+                    photo_id: photos[random].id // sorry, @dokas
+                }).then(function (res) {
+                    const imageData = res.body.photo;
+                    const url = `http://farm${imageData.farm}.staticflickr.com/${imageData.server}/${imageData.id}_${imageData.secret}.jpg`
+                    const embed = new MessageEmbed()
+                        .setFooter(`Lượt xem ${imageData.views}`)
+                        .setTimestamp(imageData.dates.posted)
+                        .setImage(url)
+                    return message.channel.send({embeds: [embed]});
+                }).catch(function (err) {
+                    return require('../../tools/functions/error')(err, message);
+                });
+            }).catch(function (err) {
+                return require('../../tools/functions/error')(err, message);
+            });
+        } catch (e) {
+            return require('../../tools/functions/error')(e, message);
+        }
+    }
+}
