c6c351b2fd1ef8f1d416787e1ed1eee50ec999e6
Thinh Nguyen <51492960+CutieCat6778@users.noreply.github.com>
2022-02-14 20:46:23 +0100

    Update guildMemberUpdate.js

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -1,5 +1,5 @@
 module.exports = (client, oldMember, newMember) => {
-  console.log(newMember.roles.cache.has("937101622847373322") && newMember.roles.cache.filter(a => a.color != "#95a5a6").size > 0);
+  console.log(newMember.roles.cache.has("937101622847373322") && newMember.roles.cache.filter(a => a.color != "#95a5a6").size > 0, newMember.roles.cache.filter(a => a.color != "#95a5a6"));
   if(newMember.roles.cache.has("937101622847373322") && newMember.roles.cache.filter(a => a.color != "#95a5a6").size > 0){
     return newMember.roles.remove('937101622847373322');
   }
