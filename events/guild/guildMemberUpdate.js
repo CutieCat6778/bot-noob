8e8fb04f696c8dc78f8848e1408810543615982c
Thinh Nguyen <51492960+CutieCat6778@users.noreply.github.com>
2022-02-14 20:57:27 +0100

    Update guildMemberUpdate.js

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -2,7 +2,7 @@ module.exports = (client, oldMember, newMember) => {
   console.log(newMember.roles.cache.has("937101622847373322") && newMember.roles.cache.filter(a => (a.color != "#95a5a6" && a.id != "937101622847373322")).size > 1);
   if(newMember.roles.cache.has("937101622847373322") && newMember.roles.cache.filter(a => (a.color != "#95a5a6" && a.id != "937101622847373322")).size > 1){
     return newMember.roles.remove('937101622847373322');
-  } else if(newMember.roles.cache.filter(a => (a.color != "#95a5a6" && a.id != "937101622847373322")).size <= 1) {
+  } else if(newMember.roles.cache.filter(a => (a.color != "#95a5a6" && a.id != "937101622847373322")).size <= 1 && !newMember.roles.cache.has("937101622847373322")) {
     return newMember.roles.add('937101622847373322');
   }
 }
