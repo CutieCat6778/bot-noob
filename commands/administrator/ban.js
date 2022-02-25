3eb794536614ec375adbe493d363a393fd50ecb6
Thinh Nguyen <51492960+CutieCat6778@users.noreply.github.com>
2022-02-25 10:15:58 +0100

    Update ban.js

--- commands/administrator/ban.js
+++ commands/administrator/ban.js
@@ -12,7 +12,10 @@ module.exports = {
 			if(!args[0]) return;
       if(args[1]){
         for(const arg of args) {
-          message.guild.members.ban(arg, {reason: `Được ban bởi ${message.member.displayName}`});
+		const id = require('mention-converter')(arg);
+		const user = message.guild.members.cache.get(id);
+		if(!user) return message.channel.send("Em lạy chị, hãy cho em đúng người mà bạn");
+          message.guild.members.ban(user.id, {reason: `Được ban bởi ${message.member.displayName}`});
         }
         return message.reply(`Đã ban: **\`${args.join('\`**, **\`')}\`**`);
       }else if(!args[1]){
